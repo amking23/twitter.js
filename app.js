3b78941cc1a0b8cfd9d2167fe2a6e7786833966a
Anne Marie King <amking23@gmail.com>
2017-06-12 17:10:39 -0500

    Add start to scripts in package.json

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+app.use(morgan('dev'));
+
+
+
+app.get('/', function(req, resp){
+	resp.send('heyyy');
+});
+
+app.get('/news', function(req, resp){
+	resp.send('this is the news');
+});
+
+app.use('/channel/special', function(request, response, next){
+	response.send('You have reached a special area');
+});
+
+app.use('/channel', function(request, response, next){
+	response.send('GET / ');
+	next();
+});
+
+app.listen(3000, function() {
+	console.log('server listening');
+});
